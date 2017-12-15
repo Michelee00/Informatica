a2348b3bcf4a72eec02f06d309723cc019d0fb0f
Michele argenti <michele.argenti000@gmail.com>
2017-12-15 08:20:28 +0100

    Update main.java

--- alberobindiricerca/main.java
+++ alberobindiricerca/main.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package alberobindiricerca;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- *
- * @author matteo.pagano
- */
 public class main {
 	public static void main(String[] args) throws FileNotFoundException {
 		AlberoBinDiRicerca albero = new AlberoBinDiRicerca();
