9dad99cbc1d14f864a6a76ba6ba8c8a3210142a8
Michele argenti <michele.argenti000@gmail.com>
2017-12-15 14:39:29 +0100

    Add files via upload

---
+++ lista/concatenata/Nodo.java
@@ -0,0 +1,16 @@
+package lista.concatenata;
+
+public class Nodo <T> {
+    T valore;
+    Nodo <T> next;
+	Nodo(T valore){
+		this.valore=valore;
+	}
+	public void setProssimo(Nodo next){
+		this.next=next;
+	}
+
+	public Nodo<T> getNext() {
+		return next;
+	}
+}
