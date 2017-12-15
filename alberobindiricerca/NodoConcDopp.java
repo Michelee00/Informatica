f8bd6f8652064ffc47ef7871311993bf49269719
Michele argenti <michele.argenti000@gmail.com>
2017-12-15 08:20:10 +0100

    Update NodoConcDopp.java

--- alberobindiricerca/NodoConcDopp.java
+++ alberobindiricerca/NodoConcDopp.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package alberobindiricerca;
 
-/**
- *
- * @author matteo.pagano
- * @param <T>
- */
 public class NodoConcDopp<T> {
 	protected T info;
 	protected NodoConcDopp<T> left;
