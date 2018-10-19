db979d23851f361296e006bab657b64cee4e1c6c
Michele argenti <michele.argenti000@gmail.com>
2018-10-19 09:36:59 +0200

    Add files via upload

---
+++ Chat/src/chat/Server.java
@@ -0,0 +1,16 @@
+package chat;
+
+import java.io.File;
+
+public class Server {
+	private String [] pizze;
+	private double [] prezzo;
+	private String [] descrizione;
+	
+	
+	//Legge il file delle pizze
+	private void leggiScrivi(){
+		File f=new File("Pizze");
+	}
+	
+}
